68f8bef361357840c9f0ce8a9ceb58c873ce6b90
davieoba <bodunrindavidbond@gmail.com>
2022-05-15 21:54:04 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const buttonStyles : React.CSSProperties = {
+    backgroundColor: '#5aac44',
+    borderRadius: '3px',
+    border:"none",
+    boxShadow: 'none',
+    height: '44px',
+    width: '132px',
+    color: '#fff',
+    fontFamily: 'avenir next regular' 
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={buttonStyles}> click me</button>
       </header>
     </div>
   );
